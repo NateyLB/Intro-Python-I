39ed7b8b78b598ab573956d2250705379a85afcb
Nathan <nateyl.b@gmail.com>
2020-06-01 15:15:07 -0700

    finished 04

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,13 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print(f'x is {x}, y is {y+.25448}, z is {z}')
 
 # Use the 'format' string method to print the same thing
-
+string = "x is {ten}, y is {two}, z is {like}"
+print(string.format(ten= x, two= (y+.25448), like = z))
 # Finally, print the same thing using an f-string
+ten = x
+two = y+.25448
+like = z
+print(f"x is {ten}, y is {two}, z is {like}")
